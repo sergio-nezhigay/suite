b80bcf897837ba5ac6a8fd01c61d4ff6a415f4b7
Sergio Nezhigay <abc1971h@gmail.com>
2024-09-11 09:44:21 +0300

    remove webpixel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   clearScreen: false,
+  esbuild: {
+    target: 'esnext',
+  },
 });
