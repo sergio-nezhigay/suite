46d781b389beaf496535ae01e4b8d7c7723b8cef
Sergio Nezhigay <abc1971h@gmail.com>
2024-11-22 18:22:34 +0200

    Implements SKU tracking and metafield updates
    Adds functionality to track SKUs and update Shopify metafields upon product creation and update actions.
    
    Introduces new logic to retrieve the last SKU from the database and increment it for new products.
    Enhances error handling when updating metafields, ensuring robustness in operations with Shopify's API.
    
    Related to SKU management improvements.

--- api/models/shopifyProduct/actions/create.js
+++ api/models/shopifyProduct/actions/create.js
@@ -4,7 +4,8 @@ import {
   save,
   ActionOptions,
 } from 'gadget-server';
-import { applyTags } from '../utils';
+
+import { applyTags } from '../../../utilities/utils';
 
 /** @type { ActionRun } */
 export const run = async ({ params, record }) => {
@@ -14,7 +15,21 @@ export const run = async ({ params, record }) => {
 };
 
 /** @type { ActionOnSuccess } */
-export const onSuccess = async ({ record, logger, api, connections }) => {
+export const onSuccess = async ({ record, api, connections }) => {
+  const shopify = connections.shopify.current;
+  const lastSKURecord = await api.lastSKU.findMany();
+  const lastSKU = lastSKURecord[0]?.value;
+  const id = lastSKURecord[0]?.id;
+  await api.lastSKU.update(id, { value: lastSKU + 1 });
+  const productId = `gid://shopify/Product/${record.id}`;
+  const result = await mutationMetafield(
+    shopify,
+    productId,
+    'custom',
+    'product_number_1',
+    lastSKU + 1
+  );
+
   if (record.body) {
     await applyTags({
       body: record.body,
@@ -28,3 +43,47 @@ export const onSuccess = async ({ record, logger, api, connections }) => {
 export const options = {
   actionType: 'create',
 };
+
+async function mutationMetafield(
+  shopify,
+  ownerId,
+  namespace,
+  key,
+  value,
+  type = 'single_line_text_field'
+) {
+  const metafieldMutation = `
+      mutation UpdateMetafield($metafields: [MetafieldsSetInput!]!) {
+        metafieldsSet(metafields: $metafields) {
+          metafields {
+            id
+            value
+          }
+          userErrors {
+            field
+            message
+          }
+        }
+      }
+    `;
+
+  const variables = {
+    metafields: [{ ownerId, namespace, key, value: String(value), type }],
+  };
+
+  try {
+    const response = await shopify.graphql(metafieldMutation, variables);
+    if (response.metafieldsSet.userErrors.length > 0) {
+      throw new Error(
+        `Metafield update failed: ${response.metafieldsSet.userErrors
+          .map((error) => `${error.field}: ${error.message}`)
+          .join(', ')}`
+      );
+    }
+
+    return response.metafieldsSet.metafields;
+  } catch (error) {
+    console.error('Error updating metafield:', error.message);
+    throw error;
+  }
+}
